bbd030c4b8b14e3044da26b4f5f2843bd9ca3801
Jahongir <tukliyev.jahongir25@gmail.com>
2022-09-21 21:04:53 +0500

    Add Postgres

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class Event(models.Model):
+    event_Image=models.ImageField(upload_to="event_images/")
+    event_Text=models.CharField(max_length=300)
+
 
 # Create your models here.
